47786629ce23fe524c1179e3a0045bf264e5734e
NarcisSt <75793612+NarcisSt@users.noreply.github.com>
2021-06-07 18:16:07 +0300

    Added README and Project finished!!

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-ProiectPADeploy
+#Proiect Proagramare Avansata 2021
+
+Autori: Narcis Stefan Barat and Maria Gabriela Coteanu
+
+###Cerinta:
+
+####Correct an address
+- Write an algorithm that corrects the fields country, state, city of a postal
+  address. Example: Country: RO, State: New York, City: Iasi will become
+  Country: RO, State: Iasi, City: Iasi
+- the algorithm needs to have unit tests and integration tests for performance
+  and precision
+- ideally the algorithm will work for all countries in the world and a few
+  languages
+- Expose a REST api using spring boot that will receive a postal address and
+  return the corrected result
+- Deploy the application as a docker container in aws/heroku or other using a
+  continuous deployment pipeline
+  
+
+
+####Pentru a rula aplicatia local, trebuie rulat serverul, si deschis portul dat in orice browser.
+
+#####De asemenea, aplicatia este si incarcata in cloud, la adresa: https://correct-adress.herokuapp.com.
+
+#####Linkul catre documentatia generata cu swagger: http://localhost:8081/swagger-ui.html
+
+#####In fiserul "app.log" din directorul "logs" sunt pastrate rezultatele tuturor rularilor.
+
+###Snippets
+
+![img.png](img.png)
+
+![img_2.png](img_2.png)
+
+![img_1.png](img_1.png)
--- src/test/java/AlgorithmTest.java
+++ src/test/java/AlgorithmTest.java
@@ -75,64 +75,85 @@ class AlgorithmTest {
 
     @Test
     void correctAddress_withIasi() {
+        //Arrange
         Algorithm algorithm = new Algorithm();
         algorithm.setField5(new String[]{"Iasi","Iasi","romania"});
-        algorithm.start();
-        LinkedList<String> newAddress = algorithm.getCorrectAddress();
         LinkedList<String> mightBeCorrect = new LinkedList<>(Arrays.asList("romania", "Iasi", "Iasi"));
 
+        //Act
+
+        algorithm.start();
+        LinkedList<String> newAddress = algorithm.getCorrectAddress();
+
+        //Assert
         assertEquals(mightBeCorrect, newAddress);
 
     }
 
     @Test
     void correctAddress_Iasi_withStreet() {
+        //Arrange
         Algorithm algorithm = new Algorithm();
         algorithm.setField5(new String[]{"strada Sf Lazar","nr 3252","bloc 3", "ap 2", "Iasi", "Iasi", "Romania"});
-        algorithm.start();
-        LinkedList<String> newAddress = algorithm.getCorrectAddress();
         LinkedList<String> mightBeCorrect = new LinkedList<>(Arrays.asList("Romania", "Iasi", "Iasi"));
 
+        //Act
+        algorithm.start();
+        LinkedList<String> newAddress = algorithm.getCorrectAddress();
+
+        //Assert
         assertEquals(mightBeCorrect, newAddress);
 
     }
 
     @Test
     void correctAddress_withMulteCuvinte() {
+        //Arrange
         Algorithm algorithm = new Algorithm();
         algorithm.setField2(new String[]{"gorj"});
         algorithm.setField4(new String[]{"ro"});
         algorithm.setField5(new String[]{"Rosia de Amaradia"});
-        algorithm.start();
-        LinkedList<String> newAddress = algorithm.getCorrectAddress();
         LinkedList<String> mightBeCorrect = new LinkedList<>(Arrays.asList("ro", "gorj", "Rosia de Amaradia"));
 
+        //Act
+        algorithm.start();
+        LinkedList<String> newAddress = algorithm.getCorrectAddress();
+
+        //Assert
         assertEquals(mightBeCorrect, newAddress);
 
     }
 
     @Test
     void correctAddress_withBacau() {
+        //Arrange
         Algorithm algorithm = new Algorithm();
         algorithm.setField5(new String[]{"Iasi","romania", "Bacau", "bacau"});
-        algorithm.start();
-        LinkedList<String> newAddress = algorithm.getCorrectAddress();
         LinkedList<String> mightBeCorrect = new LinkedList<>(Arrays.asList("romania", "Bacau", "bacau"));
 
+        //Act
+        algorithm.start();
+        LinkedList<String> newAddress = algorithm.getCorrectAddress();
+
+        //Assert
         assertEquals(mightBeCorrect, newAddress);
     }
 
     @Test
     void correctAddress_Germany_Saxony() {
+        //Arrange
         Algorithm algorithm = new Algorithm();
         algorithm.setField2(new String[]{"DE"});
         algorithm.setField3(new String[]{"Glashutte"});
         algorithm.setField4(new String[]{"7235241"});
         algorithm.setField5(new String[]{"saxony"});
-        algorithm.start();
-        LinkedList<String> newAddress = algorithm.getCorrectAddress();
         LinkedList<String> mightBeCorrect = new LinkedList<>(Arrays.asList("DE", "saxony", "Glashutte"));
 
+        //Act
+        algorithm.start();
+        LinkedList<String> newAddress = algorithm.getCorrectAddress();
+
+        //Assert
         assertEquals(mightBeCorrect, newAddress);
     }
 
